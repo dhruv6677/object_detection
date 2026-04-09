acc49bd209ea381b3a11e8b61f2b889f2dde3839
dhruv <progammerdhruv@gmail.com>
2026-04-09 23:07:35 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # object_detection
-hello guys
+
+
+## How to Run
+
+```bash
+conda create -n signlang python=3.7 -y
+```
+
+```bash
+conda activate signlang
+```
+
+```bash
+pip install  -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguage',
+    version= '0.0.0',
+    author= 'iNeuron',
+    author_email= 'progammerdhruv@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+)
